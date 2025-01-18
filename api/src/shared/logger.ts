39ec4980b10a88fa99867e5540ce3349fd3b9247
Rafaela Medina <devrafaelamedina@gmail.com>
2025-01-18 18:16:55 -0300

    Removed logs folder from Git tracking

--- api/src/shared/logger.ts
+++ api/src/shared/logger.ts
@@ -1,23 +1,10 @@
 import winston from "winston";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const logFormat = winston.format.printf(({ level, message, timestamp }) => {
-  return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
-});
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.colorize(),
-    logFormat
-  ),
+  format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/app.log", level: "info" }),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" })
   ],
 });
 
