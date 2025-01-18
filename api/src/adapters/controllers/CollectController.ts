e1c058f8e61fa5aa3ff3e5c1313c925bd1694e7d
Rafaela Medina <devrafaelamedina@gmail.com>
2025-01-18 19:41:22 -0300

    refactor(collect): remove uneccessary validations

--- api/src/adapters/controllers/CollectController.ts
+++ api/src/adapters/controllers/CollectController.ts
@@ -5,19 +5,19 @@ const CollectController = {
   async collect(req: Request, res: Response): Promise<void> {
     try {
       const { device, os, themeSwitchCount } = req.body;
-      const domain = (req as any).user?.domain; // Obtendo o domínio do token JWT
+      const domain = (req as any).user?.domain;
 
-      if (!device || !os || themeSwitchCount === undefined) {
-        res.status(400).json({ error: "All fields are required" });
-        return;
-      }
-
-      const event = await CollectEventUseCase.execute({ domain, device, os, themeSwitchCount });
+      const event = await CollectEventUseCase.execute({
+        domain,
+        device,
+        os,
+        themeSwitchCount,
+      });
       res.status(201).json(event);
     } catch (error) {
       res.status(500).json({ error: "Failed to collect data" });
     }
-  }
+  },
 };
 
 export default CollectController;
