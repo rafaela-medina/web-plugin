e8d3db8da9d6810eb97fdf7666fd1eaee9b6d816
Rafaela Medina <devrafaelamedina@gmail.com>
2025-01-19 04:44:32 -0300

    feat: create home page

--- page/src/main.tsx
+++ page/src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ThemeProvider } from '~/contexts/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
